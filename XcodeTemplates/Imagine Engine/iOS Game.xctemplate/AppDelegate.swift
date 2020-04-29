80151082a376c819f156c5fbf972ac77272d02c6
Bogdan <bogdan.baglai@jibrel.network>
2020-04-29 22:17:55 +0300

    fix Xcode Templates for Swift 5

--- XcodeTemplates/Imagine Engine/iOS Game.xctemplate/AppDelegate.swift
+++ XcodeTemplates/Imagine Engine/iOS Game.xctemplate/AppDelegate.swift
@@ -5,7 +5,7 @@ import ImagineEngine
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let scene = ___PACKAGENAME___Scene(size: UIScreen.main.bounds.size)
+        let scene = MyFirst2DGameScene(size: UIScreen.main.bounds.size)
 
         let window = GameWindow(scene: scene)
         window.makeKeyAndVisible()
